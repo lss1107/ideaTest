82a1047e2d3835f127ea97c7bfd3575e4769316e
lisong_glb <863323961@qq.com>
2020-04-21 19:29:27 +0800

    小王修改了

--- src/main/java/cn/gdut/test/gitidea/hello.java
+++ src/main/java/cn/gdut/test/gitidea/hello.java
@@ -2,6 +2,6 @@ package cn.gdut.test.gitidea;
 
 public class hello {
     public static void main(String[] args) {
-        System.out.println("aaaaaa");
+        System.out.println("aaaaaa小王修改了");
     }
 }
