c2e056735784ba3b29bdf79a8e86f9e10546a1c2
lisong_glb <863323961@qq.com>
2020-04-21 21:02:11 +0800

    hot_fix修改了内容

--- src/main/java/cn/gdut/test/gitidea/hello.java
+++ src/main/java/cn/gdut/test/gitidea/hello.java
@@ -3,5 +3,6 @@ package cn.gdut.test.gitidea;
 public class hello {
     public static void main(String[] args) {
         System.out.println("解决冲突");
+        System.out.println("fix_hot分支修改了内容");
     }
 }
