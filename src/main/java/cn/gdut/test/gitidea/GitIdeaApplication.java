b5306022f1dbf008e1c39719532178b9229b301d
lisong_glb <863323961@qq.com>
2020-04-21 18:32:20 +0800

    第一次提交

---
+++ src/main/java/cn/gdut/test/gitidea/GitIdeaApplication.java
@@ -0,0 +1,13 @@
+package cn.gdut.test.gitidea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitIdeaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitIdeaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/gdut/test/gitidea/GitIdeaApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.gdut.test.gitidea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitIdeaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
